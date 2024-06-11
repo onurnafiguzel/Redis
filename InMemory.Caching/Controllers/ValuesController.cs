013c8fa16c32342e4f1fbd18434dd6161dc8eec0
Onur Nafi Guzel <nafi.guzel@consultant.boyner.com.tr>
2024-06-11 22:48:58 +0300

    InMemory simple functions

---
+++ InMemory.Caching/Controllers/ValuesController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemory.Caching.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ValuesController : ControllerBase
+{
+	readonly IMemoryCache memoryCache;
+
+	public ValuesController(IMemoryCache memoryCache)
+	{
+		this.memoryCache = memoryCache;
+	}
+
+	[HttpGet("set/{name}")]
+	public void Set(string name) => memoryCache.Set("name", name);
+
+
+	[HttpGet]
+	public string Get()
+	{
+		if (memoryCache.TryGetValue<string>("name", out string name))
+		{
+			return name.Substring(1);
+		}
+		return string.Empty;
+
+	}
+}
--- InMemory.Caching/Program.cs
+++ InMemory.Caching/Program.cs
@@ -3,6 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddMemoryCache();
 
 var app = builder.Build();
 
