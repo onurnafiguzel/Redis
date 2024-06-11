30eb23352d29517d6c107548098293594c95a82e
Onur Nafi Guzel <nafi.guzel@consultant.boyner.com.tr>
2024-06-12 02:03:04 +0300

    Redis pub-sub example

---
+++ Redis.Pub.Sub.Subscriber.Example/Program.cs
@@ -0,0 +1,9 @@
+﻿using StackExchange.Redis;
+
+ConnectionMultiplexer connection = await ConnectionMultiplexer.ConnectAsync("localhost:1453");
+ISubscriber subscriber = connection.GetSubscriber();
+
+await subscriber.SubscribeAsync("myChannel", (channel, value) =>
+{
+	Console.WriteLine(value);
+});
